8a1f13c223ab84425526995de0cbc21774b127c7
Moon Hee <sjmh0507@naver.com>
2022-12-25 13:58:14 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.js
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.js
@@ -1,9 +1,3 @@
 function solution(n, numlist) {
-    // [false, false, 6, false, false, 9, false, false, 12]
-    return numlist.map((num) => isMultiple(n, num)).filter((item) => item !== false);
-}
-
-function isMultiple(a, b) {
-    if (b % a === 0) return b;
-    else return false;
+    return numlist.filter((num) => num % n === 0);
 }
