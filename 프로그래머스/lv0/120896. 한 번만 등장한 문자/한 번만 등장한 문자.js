1d3cfb9aba60a881ea8798cc0dcbb087b3b989ba
Moon Hee <sjmh0507@naver.com>
2022-12-26 15:28:42 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,18 +1,14 @@
 function solution(s) {
-    let obj = {};
-    let array = s.split(''); // ["a", "b", "c", "a", "b", "c", "a", "d", "c"]
+    const obj = {};
+    const answer = [];
     
-    for (let i = 0; i < s.length; i++) {
-        obj[array[i]] = obj[array[i]] === undefined ? 1 : ++obj[array[i]];    
+    for(let i = 0; i < s.length; i++){
+        obj[s[i]] = obj[s[i]] === undefined ? 1 : ++obj[s[i]];
     }
-    // [["d",1], ["a",3], ["c",3], ["b",2]]
-    const arraySort = Object.entries(obj).sort((a, b) => a[1] - b[1])
-    const arrayFilter = arraySort.filter((item) => item[1] === 1);
-    if (arrayFilter.length === 1) {
-        return arrayFilter[0][0];
-    } else if (arrayFilter.length > 1) {
-        return arrayFilter.map((item) => item[0]).sort().join('');
-    } else if (arrayFilter.length === 0) {
-        return ''
+    
+    for (let key in obj) {
+        if(obj[key] === 1) answer.push(key);
     }
+    
+    return answer.sort().join('');
 }
