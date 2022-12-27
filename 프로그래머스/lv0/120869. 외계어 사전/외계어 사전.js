6f241dec2fa2e313da540d907aa1388dfc5c5090
Moon Hee <sjmh0507@naver.com>
2022-12-27 14:36:09 +0900

    [level 0] Title: 외계어 사전, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -1,7 +1,6 @@
 function solution(spell, dic) {
-    spell = spell.sort().join('');
+    spell = spell.sort().join(''); // 'ops'
     
-    // dic을 모두 정렬하고 요소가 있다면 1 없다면 2 반환
     return dic.map((item) => item.split('').sort().join(''))
         .find((item) => item === spell) !== undefined ? 1 : 2;
 }
