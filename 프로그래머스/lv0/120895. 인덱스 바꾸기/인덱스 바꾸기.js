a01298cada497c4216b62405083605e7844ae280
Moon Hee <sjmh0507@naver.com>
2023-01-03 15:18:43 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,8 +1,9 @@
 function solution(my_string, num1, num2) {
-    let arrayStr = my_string.split(''); // ['h', 'e', 'l', 'l', 'o']
-    const one = arrayStr[num1];
-    arrayStr[num1] = arrayStr[num2];
-    arrayStr[num2] = one;
+    const [str1, str2] = [my_string[num1], my_string[num2]];
     
-    return arrayStr.join('');
+    my_string = my_string.split('');
+    my_string.splice(num1, 1, str2);
+    my_string.splice(num2, 1, str1);
+    
+    return my_string.join('');
 }
