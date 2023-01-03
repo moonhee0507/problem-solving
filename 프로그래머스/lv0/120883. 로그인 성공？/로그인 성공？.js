13e8cd4596b39fc3dadc177cf4c0886973c5322c
Moon Hee <sjmh0507@naver.com>
2023-01-03 16:15:13 +0900

    [level 0] Title: 로그인 성공?, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,11 +1,8 @@
 function solution(id_pw, db) {
-    // 아이디, 비밀번호 모두 일치하는 회원정보 존재 => 'login'
-    // 아이디 불일치 => 'fail'
-    // 아이디 일치, 비번 불일치 => 'wrong pw'
+    db = new Map(db);
+     
+    if (db.get(id_pw[0]) === id_pw[1]) return 'login';
+    if (db.get(id_pw[0]) === undefined) return 'fail';
     
-    const [id, pw] = id_pw;
-    const mapObj = new Map(db);
-    
-    // mapObj = Map(3) { 'rardss' => '123', 'yyoom' => '1234', 'meosseugi' => '1234', __proto__: {...} }
-    return mapObj.has(id) ? (mapObj.get(id) === pw ? 'login' : 'wrong pw') : 'fail';
+    return 'wrong pw';
 }
