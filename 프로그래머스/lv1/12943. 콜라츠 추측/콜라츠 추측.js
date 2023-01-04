d554c5a323a0041111aa2372964e48f58bab9729
Moon Hee <sjmh0507@naver.com>
2023-01-04 16:17:47 +0900

    Update 콜라츠 추측.js

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -3,9 +3,8 @@ function solution(num) {
     
     let count = 1;
     
-    while (count <= 500) {
-        if (num % 2 === 0) num = num / 2;
-        else if (num % 2 === 1) num = (num * 3) + 1;
+    while (count <= 500) {        
+        num = num % 2 === 0 ? num / 2 : (num * 3) + 1
         
         if (num === 1) return count;
         else count++;
