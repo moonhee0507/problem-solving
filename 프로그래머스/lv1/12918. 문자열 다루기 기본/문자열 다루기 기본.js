08f745c668ca7d1c79762d2c4155dcab2e14dbd0
Moon Hee <sjmh0507@naver.com>
2023-01-05 16:14:48 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,10 +1,5 @@
 function solution(s) {
     if (s.length === 4 || s.length === 6) {
-        s = s.split('');
-        for (let i = 0; i < s.length; i++) {
-            if (parseInt(s[i]) % 1 !== 0) return false;
-        }
-        
-        return true;
+        return /\D/g.test(s) ? false : true;
     } else return false;
 }
