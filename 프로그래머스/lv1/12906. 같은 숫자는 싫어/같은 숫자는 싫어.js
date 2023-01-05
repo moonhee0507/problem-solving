4bcb69d20ab63dc77e4a9c6b020c72dab4ad9050
Moon Hee <sjmh0507@naver.com>
2023-01-05 17:46:46 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 49.48 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,22 +1,4 @@
-function solution(arr) {
-    // [1, x, x, x, x]
-    //  i           j
-    // arr[i]와 arr[j]가 같으면 arr[j] false로 변경
-    
-    if (arr.length === 1) return arr;
-    
-    let i = 0;
-    let j = 1;
-    
-    while (j < arr.length) {
-        if (arr[i] === arr[j]) {
-            arr[j] = false;
-            j++;
-        } else {
-            j++;
-            i = j - 1;
-        }
-    }
-    
-    return arr.filter((v) => v !== false);
+function solution(arr) {    
+    // [x,1,x,3,0,x,1]
+    return arr.filter((v, i) => v !== arr[i+1]);
 }
