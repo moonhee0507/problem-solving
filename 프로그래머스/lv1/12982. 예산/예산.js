29fd0bdd83fe7e99df940cdc990a8086bd31b0ea
Moon Hee <sjmh0507@naver.com>
2023-01-06 15:09:33 +0900

    [level 1] Title: 예산, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -1,24 +1,7 @@
 function solution(d, budget) {
     d = d.sort((a, b) => a - b);
     
-    let i = 0;
-    let sum = d[0];
-    
-    // [1, 5, 8, 8, 20]
-    //           i
-    
-    // i  0  1  2   3
-    //sum 1  6  14  22
-    
-    while (true) {
-        if (sum <= budget) {
-            i++;
-            sum += d[i];
-        } else {
-            i--;
-            break;
-        }
-    }
-    
-    return i + 1;
+    return d.reduce((prev, curr) => {
+        return prev + ((budget -= curr) >= 0);
+    }, 0)
 }
