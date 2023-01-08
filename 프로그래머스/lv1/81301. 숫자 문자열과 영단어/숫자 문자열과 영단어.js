ae9542dce160aa45001e9bb04c801162f3c874f5
Moon Hee <sjmh0507@naver.com>
2023-01-08 20:23:13 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,17 +1,10 @@
 function solution(s) {
-    const enWord = { zero: '0', one: '1', two: '2', three: '3', four: '4',
-                    five: '5', six: '6', seven: '7', eight: '8', nine: '9' };
+    const numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
     
-    let temp = '';
     let answer = s;
     
-    for (let i = 0; i < s.length; i++) {
-        if (parseInt(s[i]) % 1 !== 0) temp += s[i];
-
-        if (Object.keys(enWord).includes(temp)) {
-            answer = answer.replace(temp, enWord[temp]);
-            temp = '';
-        }  
+    for (let i = 0; i < numbers.length; i++) {
+        answer = answer.split(numbers[i]).join(i);
     }
     
     return parseInt(answer);
