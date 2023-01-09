d3d8ced146c8645252741177b4962261c36f1584
Moon Hee <sjmh0507@naver.com>
2023-01-09 20:09:17 +0900

    [level 1] Title: 소수 찾기, Time: 28.50 ms, Memory: 45.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/README.md
+++ 프로그래머스/lv1/12921. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 소수 찾기 - 12921 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12921) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12921?language=javascript) 
 
 ### 성능 요약
 
-메모리: 39 MB, 시간: 90.58 ms
+메모리: 45.1 MB, 시간: 28.50 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -1,19 +1,12 @@
-function solution(n) {
-    let answer = [];
-    
-    const isPrime = (n) => {
-        for (let x of answer) {
-            if (x > Math.sqrt(n)) return true;
-            if (Number.isInteger(n / x)) return false;
+function solution(n){
+    let arr = Array(n + 1).fill(true).fill(false, 0, 2);
+    for(let i = 2 ; i * i <= n; i++){
+        if(arr[i]){
+            for(let j = i * i; j <= n; j+=i){
+                arr[j] = false;
+            }
         }
-        
-        return true;
     }
-    
-    for (let i = 2; i <= n; i++) {
-        if (!i % 2) continue;
-        if (isPrime(i)) answer.push(i);
-    }
-    
-    return answer.length;
+
+    return arr.filter((b) => b).length;
 }
