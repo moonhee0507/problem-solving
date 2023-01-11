2b8f58eb0a7464deaac7c6b406d6ee1bca944584
Moon Hee <sjmh0507@naver.com>
2023-01-11 17:21:28 +0900

    [level 1] Title: 소수 만들기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
@@ -10,7 +10,6 @@ function solution(nums) {
     }
     
     let count = 0;
-    let i = 0; let j = i + 1; let k = j + 1;
     
     for (let i = 0; i < nums.length; i++) {
         for (let j = i+1; j < nums.length; j++) {
