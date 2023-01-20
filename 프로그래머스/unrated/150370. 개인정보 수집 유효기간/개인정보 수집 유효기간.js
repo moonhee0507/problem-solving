f1549772a49da76a1e48f7fb4f626b41f48b9fa6
Moon Hee <sjmh0507@naver.com>
2023-01-21 00:08:40 +0900

    Update 개인정보 수집 유효기간.js

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,22 +1,22 @@
 function solution(today, terms, privacies) {
-  const answer = [];
-  [year, month, day] = today.split('.').map(Number);
-    
-  const todayNum = year * 12 * 28 + month * 28 + day;
-  const termObj = {};
-    
-  terms.forEach((v) => {
-    let [a, b] = v.split(" ");
-    termObj[a] = Number(b);
-  });
-    
-  privacies.forEach((v, i) => {
-    [day, term] = v.split(" ");
-    day = day.split(".").map(Number);
+    const answer = [];
+    [year, month, day] = today.split('.').map(Number);
       
-    const dates = day[0] * 12 * 28 + day[1] * 28 + day[2] + termObj[term] * 28;
-    if (dates <= todayNum) answer.push(i + 1);
-  });
-    
-  return answer;
+    const todayNum = year * 12 * 28 + month * 28 + day;
+    const termObj = {};
+      
+    terms.forEach((v) => {
+        [agreement, validity] = v.split(" ");
+        termObj[agreement] = Number(validity);
+    });
+      
+    privacies.forEach((v, i) => {
+        [getDate, agreement] = v.split(" ");
+        getDate = getDate.split(".").map(Number);
+        
+        const canHave = getDate[0] * 12 * 28 + getDate[1] * 28 + getDate[2] + termObj[agreement] * 28;
+        if (canHave <= todayNum) answer.push(i + 1);
+    });
+      
+    return answer;
 }
