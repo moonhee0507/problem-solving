26d3c3cc0919777023bb7206a32a02cc8f640f54
Moon Hee <sjmh0507@naver.com>
2023-01-20 23:34:09 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 12.70 ms, Memory: 36.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -0,0 +1,63 @@
+function solution(today, terms, privacies) {
+    terms = terms.map(v => v.split(' '));
+    
+    const termsObj = {};
+    for (let values of terms) {
+        termsObj[values[0]] = Number(values[1]);
+    }
+    
+    privacies = privacies.map(v => v.split(' ')).map(w => w.map((x, i) => {
+        if (i === 1) return termsObj[x];
+        else return x;
+    }));
+    
+    today = today.replaceAll('.', '');
+    
+    for (let i = 0; i < privacies.length; i++) {
+        const expired = afterNMonth(privacies[i][0], privacies[i][1]);
+        console.log(expired)
+        if (today.localeCompare(expired) === 1 ) privacies[i] = i + 1;
+        else privacies[i] = false;
+    }
+
+    return privacies.filter(v => v);
+}
+
+function afterNMonth(date, n) {
+    [yearC, monthC, dayC] = date.split('.').map(v => Number(v));
+    
+    dayC += n * 28 - 1;
+
+    if (dayC > 28) {
+        monthC += Math.trunc(dayC / 28);
+        dayC = dayC % 28;
+        
+        if (monthC > 12) {
+            yearC += Math.trunc(monthC / 12);
+            monthC = monthC % 12;
+        } 
+        
+        if (dayC === 0) {
+            dayC = 28;
+            monthC--;
+            
+            if (monthC === 0) {
+                monthC = 12;
+                yearC--;
+            }
+        }
+    }
+    
+    if (monthC === 0) {
+        yearC--;
+        monthC = 12;
+    }
+   
+    yearC = String(yearC);
+    if (String(monthC).length === 1) monthC = '0' + String(monthC);
+    if (String(dayC).length === 1) dayC = '0' + String(dayC);
+
+    
+ 
+    return yearC + monthC + dayC;
+}
