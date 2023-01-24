cb452e24c59f9e624c17a0115fc1ce2e9efe2f04
Moon Hee <sjmh0507@naver.com>
2023-01-24 21:50:41 +0900

    [level 2] Title: 숫자의 표현, Time: 3.98 ms, Memory: 37.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,27 @@
+function solution(n) {
+    const arrNum = Array(n).fill().map((_, i) => i + 1);
+    
+    let answer = 1;
+    // let sumPart = arrNum.slice(0, 1).reduce((prev, curr) => prev + curr, 0);
+    let sumPart = 0;
+    
+    for (let i = 0; i < n / 2; i++) {
+        if (arrNum[i] + arrNum[i+1] <= n) {
+            sumPart = arrNum.slice(i, i + 1).reduce((prev, curr) => prev + curr, 0);
+            
+            for (let j = i + 1; j < n / 2; j++) {
+                // sumPart = arrNum.slice(i, j + 1).reduce((prev, curr) => prev + curr, 0);
+                sumPart += arrNum[j];
+                
+                if (sumPart > n) break;
+                if (sumPart === n) {
+                    answer++;
+                    break;
+                }
+            }
+        } else return answer;     
+    }
+    
+    
+    return answer;
+}
