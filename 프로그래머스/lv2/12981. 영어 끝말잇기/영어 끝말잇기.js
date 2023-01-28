24008f95e010c2e181016d27e18895e595eb94e9
Moon Hee <sjmh0507@naver.com>
2023-01-28 20:43:23 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,11 +1,11 @@
 function solution(n, words) {
-    for (let i = 0; i < words.length - 1; i++) {
-        if (words[i][words[i].length - 1] === words[i+1][0]) {
-            if (words.indexOf(words[i+1]) !== words.lastIndexOf(words[i+1])) {
-                return [(words.lastIndexOf(words[i+1]) % n) + 1, Math.trunc(words.lastIndexOf(words[i+1]) / n) + 1];
-            }
-        } else return [((i + 1) % n) + 1, Math.trunc((i + 1) / n) + 1];
-    }
+    let answer = 0;
     
-    return [0, 0];
+    words.reduce((prev, curr, i) => {
+        answer = answer 
+                || ((words.slice(0, i).includes(curr) || prev !== curr[0]) ? i : answer);
+        return curr[curr.length - 1];
+    }, '');
+
+    return answer ? [answer % n + 1, Math.floor(answer / n) + 1] : [0, 0];
 }
