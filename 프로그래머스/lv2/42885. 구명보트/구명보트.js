4cca096655d5d9cb7d30a6e5fb2de352c8fac3c0
Moon Hee <sjmh0507@naver.com>
2023-01-30 22:22:11 +0900

    [level 2] Title: 구명보트, Time: 11.06 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -1,33 +1,12 @@
-// splice를 안쓰고 풀어야 효율성 테스트 통과됨
-
 function solution(people, limit) {
-    let count = 0;
+    people.sort((a, b) => a - b); 
+    // [20, 40, 60, 80, 100], 110
+    //       i   j
     
-    people = people.sort((a, b) => b - a); // [80, 70, 50, 50]
-    
-    let first = 0;
-    let second = people.length - 1;
-    
-    while (first <= second) {
-        // people[first]가 보트 제한 무게의 절반 이하가 되면, people[second]와 같이 구출시킬 수 있다
-        // 남은 사람들은 모두 나눠 탈 수 있다
-        if (people[first] <= limit / 2) {
-            count += Math.ceil((second + 1 - first) / 2);
-            break;
-        }
-        
-        count++;
-        
-        switch (people[first] + people[second] <= limit) {
-            case true:
-                first++;
-                second--;
-                break;
-            case false:
-                first++;
-                break;
-        }
+    let i = 0; // 1 2
+    for(let j = people.length - 1; i < j; j--) {
+        if(people[i] + people[j] <= limit) i++;
     }
     
-    return count;
+    return people.length - i;
 }
