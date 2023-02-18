573f59f15cdbb20e1cd0db4be2753eadbe177b74
Moon Hee <sjmh0507@naver.com>
2023-02-19 00:10:28 +0900

    [level 2] Title: 행렬의 곱셈, Time: 3.02 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -1,21 +1,9 @@
-function solution(arr1, arr2) {
-    let answer = [];
-    
-    for (let i = 0; i < arr1.length; i++) {
-        let row = [];
-        
-        for (let j = 0; j < arr2[0].length; j++) {
-            let sum = 0;
-            
-            for (let k = 0; k < arr2.length; k++) {
-                sum += arr1[i][k] * arr2[k][j];
-            }
-            
-            row.push(sum);
-        }
-        
-        answer.push(row);
-    }
-    
-    return answer;
+const solution = (arr1, arr2) => {
+    return arr1.map((row) => {
+        return arr2[0].map((_, cIdx) => {
+            return row.reduce((prev, curr, rIdx) => {
+                return prev + (curr * arr2[rIdx][cIdx]);
+            }, 0)
+        })
+    });
 }
