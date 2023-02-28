36a90b6baa31a6c847ea4f01a1f6bf0d00d68d5e
Moon Hee <sjmh0507@naver.com>
2023-03-01 00:53:48 +0900

    [level 2] Title: 프린터, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프린터/프린터.js
+++ 프로그래머스/lv2/42587. 프린터/프린터.js
@@ -1,18 +1,19 @@
 function solution(priorities, location) {
-    let max;
-    let cnt = 0;
-    
-    while(true) {
-      	max = Math.max.apply(null, priorities);
-        let n = priorities.shift();
-        
-        if (n === max) {
-            cnt++;
-            if (location === 0) return cnt;
-        } else priorities.push(n);
-        
-        location--;
-        
-        if (location == -1) location = priorities.length - 1;
+    let list = priorities.map((v,i)=>({
+        my : i === location,
+        val : v
+    }));
+  
+    let count = 0;        
+  
+    while(true){
+        const cur = list.splice(0,1)[0];        
+        // cur.val보다 큰 값이 하나라도 있으면
+        if(list.some(el => el.val > cur.val )){
+            list.push(cur);                        
+        } else {            
+            count++;
+            if(cur.my) return count;
+        }
     }
 }
