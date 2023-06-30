311013bdb17f05a8677193b7dcb263f24a838401
moon hee <sjmh0507@naver.com>
2023-06-30 15:51:13 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 115.40 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,8 +1,20 @@
 function solution(participant, completion) {
-    let sum = participant.concat(completion);
-    sum.sort();
+    let answer = "";
     
-    for (let i = 0; i < sum.length; i += 2) {
-        if (sum[i] !== sum[i+1]) return sum[i];
+    let map = new Map();
+    
+    for (let p of participant) {
+        map.set(p, map.get(p) + 1 || 1);
     }
+    
+    for (let c of completion) {
+        if (map.get(c) === 1) map.delete(c);
+        else map.set(c, map.get(c) - 1);
+    }
+    
+    for (let [key, value] of map) {
+        answer = key;
+    }
+    
+    return answer;
 }
